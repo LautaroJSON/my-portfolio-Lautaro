5a3b552284a084d001d2c1e343d91711b879f2ce
LautaroJSON <fernandez.n.lautaro@gmail.com>
2022-12-21 07:14:37 -0300

    perf: add head next component with title page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
+import Head from "next/head"
 import React from "react"
 import Layout from "../layout"
+import "./globalStyles.css"
 
 const _app = ({ Component, pageProps, router }) => {
   return (
     <Layout>
+      <Head>
+        <title>Lautaro&apos;s Homepage</title>
+      </Head>
       <Component {...pageProps} key={router.route} />
     </Layout>
   )
