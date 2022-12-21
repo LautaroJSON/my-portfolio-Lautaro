4227b67fc53b04396a5aceeffaee65ace23653b0
LautaroJSON <fernandez.n.lautaro@gmail.com>
2022-12-21 07:14:06 -0300

    perf: add layout

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,11 +1,18 @@
 import React from "react"
+import Header from "../components/header"
+import { LayoutContainer } from "./styles"
 
 const Layout = ({ children }) => {
+  const SECTIONS_HEADER: any = [
+    { name: "Home" },
+    { name: "About me" },
+    { name: "Contact" },
+  ]
   return (
-    <>
-      <div>Layout</div>
+    <LayoutContainer>
+      <Header sections={SECTIONS_HEADER} />
       <div>{children}</div>
-    </>
+    </LayoutContainer>
   )
 }
 
