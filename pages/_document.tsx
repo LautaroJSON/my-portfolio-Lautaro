9b1f8a56b5d9c0829aa006a4a52aedd92590a484
LautaroJSON <fernandez.n.lautaro@gmail.com>
2022-12-29 23:35:51 -0300

    fix: delete viewport

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document"
 
-export default function Document() {
+function Document() {
   return (
-    <Html>
-      <Head />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0"
-      ></meta>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Chivo+Mono:wght@400;500;600&family=Open+Sans:wght@300;400;500;600;700&display=swap"
-        rel="stylesheet"
-      />
+    <Html lang="es">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Chivo+Mono:wght@400;500;600&family=Open+Sans:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -21,3 +18,5 @@ export default function Document() {
     </Html>
   )
 }
+
+export default Document
