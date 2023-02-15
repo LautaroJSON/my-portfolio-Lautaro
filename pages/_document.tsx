a01e38349187b6503f3dc59db3d29cdd4b16a921
LautaroJSON <fernandez.n.lautaro@gmail.com>
2023-02-15 20:54:02 -0300

    perf: change _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,10 @@ function Document() {
           href="https://fonts.googleapis.com/css2?family=Chivo+Mono:wght@400;500;600&family=Open+Sans:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
