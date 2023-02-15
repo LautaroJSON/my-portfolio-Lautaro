51d2b6a85e0faa3a9334f532bef738ef9e694043
LautaroJSON <fernandez.n.lautaro@gmail.com>
2023-02-15 20:50:56 -0300

    perf: new logo

--- components/logo/styles.tsx
+++ components/logo/styles.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components"
 
 export const LogoStyled = styled.div`
-  width: 56px;
-  height: 56px;
+  width: 46px;
+  height: 46px;
 
   box-sizing: border-box;
   border-radius: 6px;
@@ -10,17 +10,28 @@ export const LogoStyled = styled.div`
 
   position: relative;
 
-  transition: all 0.8s;
+  transition: all 0.6s;
 
   &:before {
     content: "L";
-    font-size: 2.4rem;
+    font-size: 2rem;
     font-weight: 600;
-    position: absolute;
     color: black;
 
+    position: absolute;
     bottom: -5px;
-    right: 6px;
+    right: 5px;
+  }
+
+  &:after {
+    content: "autaro Fernandez.";
+    font-size: 2rem;
+    font-weight: 500;
+    white-space: nowrap;
+
+    position: absolute;
+    bottom: -5px;
+    left: 50px;
   }
 
   &:hover {
@@ -33,13 +44,16 @@ export const LogoStyled = styled.div`
   }
 
   @media (max-width: 1045px) {
+    width: 42px;
+    height: 42px;
+
     &:before {
-      content: "L";
       font-size: 2.2rem;
-      font-weight: 400;
+      font-weight: 600;
     }
 
-    width: 50px;
-    height: 50px;
+    &:after {
+      display: none;
+    }
   }
 `
